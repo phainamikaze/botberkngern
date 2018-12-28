19ee8c285758d8f55b30c8f0efb23a8e4af1f71c
WITSANU_BO\Witsanu <phai_namikaze@hotmail.com>
2018-12-28 21:03:46 +0700

    createlist ui ok

--- src/components/Formlist.jsx
+++ src/components/Formlist.jsx
@@ -5,32 +5,39 @@ import { ButtonArea,
   CellsTips,
   CellHeader,
   CellBody,
-  Form,
   FormCell,
   Input,
   Label,
-  Page
+  Page,
+  CellFooter,
+  Icon
 } from 'react-weui';
 const Formlist = (props) => {
+    
     return (
         <Page transition={false}>
-            <CellsTitle>สร้างลิสต์</CellsTitle>
-            <Form>
-                <FormCell>
-                    <CellHeader>
-                        <Label>ลิสต์</Label>
-                    </CellHeader>
-                    <CellBody>
-                        <Input type="text" placeholder="กรอกชื่อลิสต์" id="listtitle"/>
-                    </CellBody>
-                </FormCell>
-            </Form>
+        {/* Empty action attr enables 'Go' Submit Button on iOS Keyboard */}
+            <form action="true" onSubmit={props.onSubmit} >
+            <CellsTitle>สร้างลิสต์</CellsTitle>   
+            <FormCell>
+                <CellHeader>
+                    <Label>ลิสต์</Label>
+                </CellHeader>
+                <CellBody>
+                    <Input type="text" placeholder="กรอกชื่อลิสต์" />
+                </CellBody>
+                <CellFooter>
+                    <Icon value="warn" />
+                </CellFooter>
+            </FormCell>
+            
             <CellsTips>กรุณากรอกชื่อลิสต์</CellsTips>
             <ButtonArea>
-                <Button onClick={() => props.createnewlist(document.getElementById('listtitle'))}>
+                <Button>
                     OK
                 </Button>
             </ButtonArea>
+            </form>
         </Page>
     );
 };
--- src/containers/Createlist.jsx
+++ src/containers/Createlist.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Formlist from '../components/Formlist';
 class Createlist extends React.Component {
+    savenewlist=(event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        //const data = new FormData(event.target);
+        console.log(event.target[0].value);
+      };
     render(){
         return(
-            <Formlist/>
+            <Formlist onSubmit={this.savenewlist.bind(this)}/>
         );
     }
+
 }
 
 export default Createlist
