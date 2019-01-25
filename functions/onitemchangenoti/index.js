9229049b704d6077131fc9b495f03b4e6195cf0e
phai <phai_namikaze@hotmail.com>
2019-01-25 14:27:54 +0700

    noti msg ok

--- functions/onitemchangenoti/index.js
+++ functions/onitemchangenoti/index.js
@@ -1,4 +1,9 @@
 'use strict';
+const AWS = require('aws-sdk');
+
+const LIST_TABLE = process.env.LIST_TABLE;
+const AWS_REGION = process.env.AWS_REGION;
+const docClient = new AWS.DynamoDB.DocumentClient({region: AWS_REGION});
 const helpers = require('messenger-api-helpers');
 const sendApi = helpers.send;
 module.exports.handler = (event, context,callback) => {
@@ -7,7 +12,6 @@ module.exports.handler = (event, context,callback) => {
   event.Records.forEach(record => {
     result.push(new Promise((resolve,reject)=>{
       const ownlist = record.dynamodb.Keys.ownlist.S;
-      const createtimeItem = record.dynamodb.Keys.createtime.S;
       const [owner,createtime] = ownlist.split("_");
       if(record.eventName==="MODIFY"){
         if(record.dynamodb.NewImage.status.S === record.dynamodb.OldImage.status.S){
@@ -29,7 +33,11 @@ module.exports.handler = (event, context,callback) => {
           if(recipientId===null){
             resolve();
           }else{
-            sendApi.sendItemUpdated(recipientId, owner, createtime, 'title').then(()=>{
+            getListname(owner,createtime).then((list)=>{
+              return list.title;
+            }).then((listname)=>{
+              return sendApi.sendItemUpdated(recipientId, owner, createtime,listname,record.dynamodb.NewImage.details.S)
+            }).then(()=>{
               resolve();
             });
           }
@@ -45,3 +53,22 @@ module.exports.handler = (event, context,callback) => {
     callback(null)
   });
 };
+
+const getListname = (owner,createtime)=>{
+  return new Promise((resolve,reject)=>{
+    const params = {
+      TableName: LIST_TABLE,
+      Key: {
+        owner: owner,
+        createtime: createtime
+      },
+    };
+    docClient.get(params,function(err,data){
+      if(err){
+        resolve('ไม่มีชื่อ');
+      }else{
+        resolve(data.Item);
+      }
+    });
+  });
+}
